878e0e93bc775a6faecef1f43b96406116e90ab8
Zahra Rauf <zahra.aleem@gmail.com>
2025-09-28 18:28:26 +0200

    Book class defined with author_id as foreign key

--- data_models.py
+++ data_models.py
@@ -1,4 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, Column, Integer, String
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
@@ -6,14 +7,31 @@ db = SQLAlchemy()
 class Author(db.Model):
     __tablename__ = 'authors'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
+    author_id = Column(Integer, primary_key=True, autoincrement=True)
+    author_name = Column(String)
     birth_date = Column(String)
     date_of_death = Column(String)
 
     def __repr__(self):
-        return f"Author(id = {self.id}, name = {self.name})"
+        return f"Author(id = {self.author_id}, name = {self.author_name})"
 
     def __str__(self):
-        return f"The id {self.id} represents the author {self.name}"
+        return f"The id {self.author_id} represents the author {self.author_name}"
+
+# Defining class Books
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    book_id = Column(Integer, primary_key=True, autoincrement=True)
+    isbn = Column(String)
+    book_title = Column(String)
+    publication_year = Column(Integer)
+    author_id = Column(Integer, ForeignKey("authors.id"))
+
+    def __repr__(self):
+        return f"Book(id = {self.book_id}, title = {self.book_title})"
+
+    def __str__(self):
+        return f"The book {self.book_id} is written by {self.author_id}"
+
 
